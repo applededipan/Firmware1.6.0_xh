727acaf11f8809dd957c4826c42969734d88071e
yhb <yhb@brainybeeuav.com>
2017-02-17 22:37:46 +0800

    add handle_message_rc_channels message to extend the rcchanenls from 8 to 14, and use time_boot_ms as targetID
    add handle_message_rc_channels message to extend the rcchanenls from 8
    to 14, and use time_boot_ms as targetID

--- src/modules/mavlink/mavlink_receiver.h
+++ src/modules/mavlink/mavlink_receiver.h
@@ -133,6 +133,7 @@ private:
 	void handle_message_radio_status(mavlink_message_t *msg);
 	void handle_message_manual_control(mavlink_message_t *msg);
 	void handle_message_rc_channels_override(mavlink_message_t *msg);
+	void handle_message_rc_channels(mavlink_message_t *msg);//sujun
 	void handle_message_heartbeat(mavlink_message_t *msg);
 	void handle_message_ping(mavlink_message_t *msg);
 	void handle_message_request_data_stream(mavlink_message_t *msg);
